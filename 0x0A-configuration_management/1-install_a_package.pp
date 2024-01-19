62fa38330508477a7685c5dcdb7bff79d29bd813
zakaria <grarizakaria@gmail.com>
2024-01-19 15:42:45 +0100

    alx

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
-# install puppet-lint -v 2.5.0
+# 1-install_a_package.pp
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Define package resource for Flask with version 2.1.0 and specify Werkzeug version
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['Werkzeug'],
 }
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',  # Specify the version of Werkzeug
+  provider => 'pip3',
+}
+
+# End of Puppet manifest
