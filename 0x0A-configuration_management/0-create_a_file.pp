046da97891515ac86f414941eeb3df42abfd0b53
zakaria <grarizakaria@gmail.com>
2024-01-19 15:25:47 +0100

    alx

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# File: create_school_file.pp
+
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+0x0A-configuration_management0x0A-configuration_management0x0A-configuration_management0x0A-configuration_management
