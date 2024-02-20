6fd3e6e82482d6af227244e747771d2817a7984a
zakaria <grarizakaria@gmail.com>
2024-02-20 10:09:25 +0100

    alx

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" """
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(employee_id))
+    user_data = user.json()
+    name = user_data.get("name")
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    user_todos = [todo for todo in todos if todo['userId'] == int(employee_id)]
+    csv_file_name = '{}.csv'.format(employee_id)
+    with open(csv_file_name, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        for todo in user_todos:
+            csv_writer.writerow([employee_id, name, todo['completed'], todo['title']])
