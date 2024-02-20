f76b2ee75b0059309ac94a4e2585e328da9a55a1
zakaria <grarizakaria@gmail.com>
2024-02-20 10:51:03 +0100

    alx

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     for user in users:
         employee_id = user['id']
         username = user['username']
-        user_tasks = [{"username": username, "task": todo['title'], 
-                       "completed": todo['completed']} 
+        user_tasks = [{"username": username, "task": todo['title'],
+                       "completed": todo['completed']}
                       for todo in todos if todo['userId'] == employee_id]
         all_employee_tasks[str(employee_id)] = user_tasks
 
