00cae702c402a6e3da1aa71e1b1f4794b361b8fc
zakaria <grarizakaria@gmail.com>
2024-03-05 19:59:16 +0100

    alx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ task 0 """
-import requests
 
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit."""
+    import requests
+    
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "My-User-Agent"}
 
