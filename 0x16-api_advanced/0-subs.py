de46ef1c1c346caf06fb16f62be120507e18ec8a
zakaria <grarizakaria@gmail.com>
2024-03-05 20:40:22 +0100

    alx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" task 0 """
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
-    to the subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
+    to the subreddit"""
+    import requests
 
-    sub_info = requests.get(url, headers=headers, allow_redirects=False)
-
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
     if sub_info.status_code >= 300:
         return 0
 
